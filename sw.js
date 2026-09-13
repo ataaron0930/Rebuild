729a4e309954e2feec3ca1295616f213856efafb
ataaron0930 <aldru.t.aaron@me.com>
2026-09-13 10:23:35 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'protein-count-v3';
+var CACHE = 'protein-count-v4';
 var FILES = ['./', './index.html', './manifest.webmanifest', './apple-touch-icon.png', './icon-512.png'];
 
 self.addEventListener('install', function(e){
